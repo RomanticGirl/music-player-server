d063cb55c13dcb0a43e85abefa6532331ca6b4da
mrudnev <pleasegivemeonechance@mail.ru>
2023-04-24 14:49:28 +0300

    server process

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,20 @@
 import { Module } from '@nestjs/common';
 import { TrackModule } from './track/track.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { FileModule } from './file/file.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import * as path from 'path';
 
 @Module({
   imports: [
+    ServeStaticModule.forRoot({
+      rootPath: path.resolve(__dirname, 'static'),
+    }),
     MongooseModule.forRoot(
       'mongodb+srv://runningthroughwoodland:1437@cluster0.koks5ug.mongodb.net/?retryWrites=true&w=majority',
     ),
     TrackModule,
+    FileModule
   ],
 })
-export class AppModule {}
+export class AppModule { }
--- src/track/track.controller.ts
+++ src/track/track.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Delete, Get, Param, Post, UploadedFiles, UseInterceptors } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Query, UploadedFiles, UseInterceptors } from '@nestjs/common';
 import { TrackService } from './track.service';
 import { CreateTrackDto } from './dto/create-track.dto';
 import { ObjectId } from 'mongoose';
@@ -19,10 +19,11 @@ export class TrackController {
         return this.trackService.create(dto, picture[0], audio[0]);
     }
 
-    @Get()
-    getAll() {
-        return this.trackService.getAll();
+    @Get('/search')
+    search(@Query('query') query: string) {
+        return this.trackService.search(query);
     }
+
     @Get(':id')
     getOne(@Param('id') id: ObjectId) {
         return this.trackService.getOne(id);
@@ -35,5 +36,10 @@ export class TrackController {
     @Post('/comment')
     addComment(@Body() dto: CreateCommentDto) {
         return this.trackService.addComment(dto);
+    }
+
+    @Post('/listen/:id')
+    listen(@Param('id') id: ObjectId) {
+        return this.trackService.listen(id);
     }
 }
